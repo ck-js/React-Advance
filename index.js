8e3cb3c438d3cd5c7b79be8ebfae751ba5e5d289
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-04-23 22:39:54 +0700

    Add styling to our react component with the class name attribute, we style our container and also input. Add component that takes user input then triggers the input on change attribute which calls the number handler that then calls the event target value through the state function set number, then goes through our if else conditional statement and passes a string value to our response variable. At the same time our state number was updated which gave the if else operation a value to compare. We then display our result in a p element using embedded expression to display the state number and also the operation result string value.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -30,10 +30,34 @@ const App2 = () => {
   )
 }
 
+const App3 = () => {
+  const [number, setNumber] = useState(420);
 
+  const handleNumber = (event) => {
+    setNumber(event.target.value)
+  }
+let response;
+if (number % 2 == 1) {
+response = "odd";
+}else {
+  response = "even";
+}
 
+return (
+  <div className="container">
+    <h1>Even Odd Detector v2</h1>
+    <input className="number"
+    type="number"
+    value={number}
+    onChange={handleNumber}
+    /> 
+    <p>The number {number} is an {response}
+    number</p>
+  </div>
+)
+}
 ReactDOM.render(
-<App2
+<App3
 />,
 document.getElementById("root")
 )
