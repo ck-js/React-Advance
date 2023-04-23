f1a67635df6f878bfe7ff2f794926b3b73ee0a4c
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-04-23 21:33:15 +0700

    Add basic style and use style name in react component to style oir html. Create even odd detector. First we store a number in variable then we create logic and store the logics result in variable, if number % 2 == 1 then the response is odd and else its even. Then display our result in return method using embedded expressions.

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
 const App1 = () => {
   return (
-    <div>
+    <div className="container">
       <h1>React Advanced Test</h1>
+    </div>
+  )
+}
+
+const App2 = () => {
+  const number = 99;
+
+  let response;
+  if (number % 2 == 1) {
+    response = "odd";
+  } else {
+    response = "even";
+  }
+
+  return (
+    <div className="container">
+      <h1>Even Odd Detector</h1>
+      <p>The number {number} is an {response}
+       number</p>
     </div>
   )
 }
@@ -13,7 +33,7 @@ const App1 = () => {
 
 
 ReactDOM.render(
-<App1
+<App2
 />,
 document.getElementById("root")
 )
