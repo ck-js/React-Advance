df6287b2239a0e7d073c7cff983ba3d110a3540b
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-04-23 20:53:05 +0700

    Add react folers and files for react advance conecpts with boiler plate code and confirm react is rendering properly.

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App1 = () => {
+  return (
+    <div>
+      <h1>React Advanced Test</h1>
+    </div>
+  )
+}
+
+
+
+ReactDOM.render(
+<App1
+/>,
+document.getElementById("root")
+)
